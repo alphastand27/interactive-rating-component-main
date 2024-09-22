473baf6723dd5c09bbb89e178b6e0db3cee27fa8
Micheal <micksilwimba867@gmail.com>
2024-09-22 11:43:46 +0200

    js build

--- index.js
+++ index.js
@@ -1,32 +1,96 @@
+//main display container
 const container = document.getElementById("container");
+
+//svg src
 const starIcon = "images/icon-star.svg";
 const illustration = "images/illustration-thank-you.svg";
 
-const intro = `
-    <img id="star-icon" src="${starIcon}" alt="star icon">
-    <h1 id="question">How did we do?</h1>
-    <p id="details">
-        Please let us know how we did with your support request. All feedback is appreciated 
-        to help us improve our offering!
-    </p>
-    <section id="rating" class="container text-center">
-        <div class="rates">1</div>
-        <div class="rates">2</div>
-        <div class="rates">3</div>
-        <div class="rates">4</div>
-        <div class="rates">5</div>
-    </section>
-    <button id="button" type="submit" class="container text-center">SUBMIT</button>
+//image#star-icon
+const star = document.getElementById("star-icon");
+
+//displays elements object for the outro 
+const outObject = {
+    img: `<img id="illustration" src="${illustration}" alt="a thank you illustration">`,
+    h1: `<h1 id="thanks">Thank you!</h1>`,
+    selected: `<p id="selected">You selected 0 out of 5</p>`,
+    detailed: 
+        `<p id="detailed-thanks">
+            We appreciate you taking the time to give a rating. If you ever need more support, 
+            don’t hesitate to get in touch!
+        </p>`
+};
+
+//divs and button tags
+const rates = document.getElementsByClassName("rates");
+const button = document.getElementById("button");
+
+//setting the source for the image tag in the index.html
+star.src = starIcon;
+
+//outro display for the innerHTML
+let outro = `
+    ${outObject.img}
+    ${outObject.h1}        
+    ${outObject.selected}
+    ${outObject.detailed}
 `;
 
-const outro = `
-    <img id="illustration" src="${illustration}" alt="a thank you illustration">
-    <h1 id="thanks">Thank you!</h1>
-    <p id="selected">You selected 4 out of 5</p>
-    <p id="detailed-thanks">
-        We appreciate you taking the time to give a rating. If you ever need more support, 
-        don’t hesitate to get in touch!
-    </p>
-`;
+// function for the rating and rating display
+const ratingLogic = (index) => {
 
-container.innerHTML = intro;
+    //checks if the color default is set in the html/css not JS
+    if (rates[index].style.color === "") {
+        //if the default was set in html/css then it is changes to the below specified styles
+        rates[index].style = "color: var(--Dark-Blue); background-color: var(--White)";
+        //Which also updates the text for the p#selected tag to display the rating picked out of five
+        outObject.selected = `<p id="selected">You selected ${index + 1} out of 5</p>`;
+        //and the outro display is set
+        return outro = `
+            ${outObject.img}
+            ${outObject.h1}        
+            ${outObject.selected}
+            ${outObject.detailed}
+        `;
+    //checks if the color is set in JS not html/css
+    } else {
+        //then changes the color back to the default set in html/css
+        rates[index].style.color = "";
+        //also changes the background back to the default set in html/css
+        rates[index].style.backgroundColor = "";
+        //Which also updates the text for the p#selected tag to display the rating picked out of five
+        outObject.selected = `<p id="selected">You selected 0 out of 5</p>`;
+        //and the outro display is set
+        return outro = `
+            ${outObject.img}
+            ${outObject.h1}        
+            ${outObject.selected}
+            ${outObject.detailed}
+        `;
+    };
+};
+
+//click events handling for the ratings 1 to 5
+rates[0].addEventListener("click", () => {
+    ratingLogic(0);  
+});
+
+rates[1].addEventListener("click", () => {
+    ratingLogic(1);  
+});
+
+rates[2].addEventListener("click", () => {
+    ratingLogic(2);  
+});
+
+rates[3].addEventListener("click", () => {
+    ratingLogic(3);  
+});
+
+rates[4].addEventListener("click", () => {
+    ratingLogic(4);  
+});
+
+//submit button click event handling
+button.addEventListener("click", () => {
+    container.innerHTML = outro;
+})
