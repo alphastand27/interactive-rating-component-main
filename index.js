cd621518fa682ec1e35a5bda4629d72f5d15c533
Micheal <micksilwimba867@gmail.com>
2024-10-14 19:18:13 +0200

    js fix

--- index.js
+++ index.js
@@ -35,24 +35,44 @@ let outro = `
     ${outObject.detailed}
 `;
 
+//boolean variable to keep track of the state of the clicked button
+let clickedState;
+
 // function for the rating and rating display
 const ratingLogic = (index) => {
 
     //checks if the color default is set in the html/css not JS
     if (rates[index].style.color === "") {
-        //if the default was set in html/css then it is changes to the below specified styles
-        rates[index].style = "color: var(--Dark-Blue); background-color: var(--White)";
-        //Which also updates the text for the p#selected tag to display the rating picked out of five
-        outObject.selected = `<p id="selected">You selected ${index + 1} out of 5</p>`;
-        //and the outro display is set
-        return outro = `
-            ${outObject.img}
-            ${outObject.h1}        
-            ${outObject.selected}
-            ${outObject.detailed}
-        `;
+        //checks if there is a clicked button and if there is the function is exited changing nothing 
+        if(clickedState === true) {
+            return;
+        //checks if there is no clicked button and if there is not the else block is executed
+        } else {
+            /*
+            ** set the button click state to true(indicating it is clicked) 
+            ** therefore preventing other buttons from being clickable
+            */
+            clickedState = true;
+            //if the default was set in html/css then it is changes to the below specified styles
+            rates[index].style = "color: var(--Dark-Blue); background-color: var(--White)";
+            //Which also updates the text for the p#selected tag to display the rating picked out of five
+            outObject.selected = `<p id="selected">You selected ${index + 1} out of 5</p>`;
+            //and the outro display is set
+            return outro = `
+                ${outObject.img}
+                ${outObject.h1}        
+                ${outObject.selected}
+                ${outObject.detailed}
+            `;
+        }
     //checks if the color is set in JS not html/css
     } else {
+        /*
+        ** when the button is clicked for a second time 
+        ** the state is changed from true to false
+        ** allowing another button to be clickable
+        */
+        clickedState = false;
         //then changes the color back to the default set in html/css
         rates[index].style.color = "";
         //also changes the background back to the default set in html/css
@@ -71,7 +91,7 @@ const ratingLogic = (index) => {
 
 //click events handling for the ratings 1 to 5
 rates[0].addEventListener("click", () => {
-    ratingLogic(0);  
+    ratingLogic(0);
 });
 
 rates[1].addEventListener("click", () => {
